a024f5dd0cf232f3c6b8c52de485820250a853e4
Prince Anyomi <anyomiprince27@gmail.com>
2022-09-11 15:48:33 -0700

    Task 5 Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-
-	    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+       	return (0);
 
 }
