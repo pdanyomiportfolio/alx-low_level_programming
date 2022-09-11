9d2a2503d1404c4c3c4c06273f8682a4fe9ce358
Prince Anyomi <anyomiprince27@gmail.com>
2022-09-11 15:21:33 -0700

    Task Done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,21 @@
-include
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    return (0);
+
+}
