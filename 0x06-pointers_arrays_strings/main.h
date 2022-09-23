dcd3fe50fbec24fef8088fbbd17d5edac8ee2e33
Prince Anyomi <anyomiprince27@gmail.com>
2022-09-22 22:24:43 -0700

    Taskk 0 Done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
-
 
 int _putchar(char c);
 
@@ -29,7 +26,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
-
-
 
 #endif
